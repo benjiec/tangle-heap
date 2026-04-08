60e444b618bacdd82a77ceb7b6ef273ab6f2e443
Benjie Chen <benjie@alum.mit.edu>
2026-04-08 07:10:55 -0400

    filter many smaller files

--- scripts/ko-filter-target.py
+++ scripts/ko-filter-target.py
@@ -1,13 +1,14 @@
 import os
+import shutil
 import argparse
 from heap.ko import filter_detected_by_target_length
 
 if __name__ == "__main__":
 
     ap = argparse.ArgumentParser()
-    ap.add_argument("hmm_result_tsv")
-    ap.add_argument("filtered_tsv")
+    ap.add_argument("hmm_result_tsv", nargs="+")
     ap.add_argument("--match-to-threshold-ratio-min", default=0.5, type=float)
+    ap.add_argument("--forget-original", action="store_true", default=False)
     args = ap.parse_args()
 
     db_dir = os.environ.get("HMM_DB_DIR")
@@ -18,4 +19,9 @@ if __name__ == "__main__":
     if not os.path.exists(ko_threshold):
         raise Exception(f"Cannot find KO threshold TSV at {ko_threshold}")
 
-    filter_detected_by_target_length(args.hmm_result_tsv, ko_threshold, args.filtered_tsv, min_match_to_threshold_ratio=args.match_to_threshold_ratio_min)
+    for tsv_fn in args.hmm_result_tsv:
+        if args.forget_original is False:
+            orig_fn = tsv_fn+".orig"
+            shutil.copy(tsv_fn, orig_fn)
+        filter_detected_by_target_length(tsv_fn, ko_threshold, tsv_fn, min_match_to_threshold_ratio=args.match_to_threshold_ratio_min)
+        print(tsv_fn)
